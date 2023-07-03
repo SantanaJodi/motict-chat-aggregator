ffb5865c1e93c92e5b96fa7de30a7fc822152ea9
anggihnurh <works.anggihnur@gmail.com>
2023-07-03 22:19:09 +0700

    add some outline icons

--- public/icons/outline/index.ts
+++ public/icons/outline/index.ts
@@ -1,6 +1,8 @@
 import BroadcastIcon from "./BroadcastIcon";
 import ChartIcon from "./ChartIcon";
 import ChatIcon from "./ChatIcon";
+import ChevronLeft from "./ChevronLeft";
+import ChevronRight from "./ChevronRight";
 import HelpIcon from "./HelpIcon";
 import NotificationIcon from "./NotificationIcon";
 import PeoplesIcon from "./PeoplesIcon";
@@ -8,6 +10,8 @@ import ProfileIcon from "./ProfileIcon";
 import SettingsIcon from "./SettingsIcon";
 
 export {
+  ChevronLeft,
+  ChevronRight,
   ChatIcon,
   BroadcastIcon,
   ChartIcon,
