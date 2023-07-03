cf0d1b210bf24b769c568136c681c0a2f02d2543
anggihnurh <works.anggihnur@gmail.com>
2023-07-03 13:40:05 +0700

    added outline icons & images logo assets

---
+++ public/icons/outline/index.ts
@@ -0,0 +1,19 @@
+import BroadcastIcon from "./BroadcastIcon";
+import ChartIcon from "./ChartIcon";
+import ChatIcon from "./ChatIcon";
+import HelpIcon from "./HelpIcon";
+import NotificationIcon from "./NotificationIcon";
+import PeoplesIcon from "./PeoplesIcon";
+import ProfileIcon from "./ProfileIcon";
+import SettingsIcon from "./SettingsIcon";
+
+export {
+  ChatIcon,
+  BroadcastIcon,
+  ChartIcon,
+  PeoplesIcon,
+  SettingsIcon,
+  HelpIcon,
+  NotificationIcon,
+  ProfileIcon,
+};
