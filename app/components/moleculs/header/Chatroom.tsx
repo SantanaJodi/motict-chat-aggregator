d43a3d4bebe5656b0c65a61d919566eda9fe885c
anggihnurh <works.anggihnur@gmail.com>
2023-07-05 11:20:25 +0700

    remove component to another folder

--- app/components/moleculs/header/Chatroom.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export const Chatroom: React.FC<ChatroomProps> = () => {
-  return <h1>Chatroom</h1>;
-};
-
-export interface ChatroomProps {
-  isLoading?: boolean;
-}
