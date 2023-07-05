05987f553671ed5c6ff2f5fc169d98610b3d269e
anggihnurh <works.anggihnur@gmail.com>
2023-07-05 11:19:04 +0700

    add input component

---
+++ app/components/atoms/Input.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+interface InputProps {
+  placeholder: string;
+  value: string;
+  onChange: (value: string) => void;
+  Icon?: React.ElementType;
+}
+
+const Input: React.FC<InputProps> = ({
+  onChange,
+  placeholder,
+  value,
+  Icon,
+}) => {
+  return (
+    <div className="bg-[#EEF5FF] rounded-lg flex flex-row items-center gap-2 p-2">
+      {Icon && <Icon />}
+      <input
+        className="w-full bg-[#EEF5FF] border-none focus:outline-none text-base placeholder:text-[#AABDD7]"
+        value={value}
+        onChange={(e) => onChange(e.target.value)}
+        placeholder={placeholder}
+      />
+    </div>
+  );
+};
+
+export default Input;
