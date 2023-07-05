a2acc061989f156f554a06444b348cb051f401ee
anggihnurh <works.anggihnur@gmail.com>
2023-07-05 11:18:35 +0700

    add avatar component

---
+++ app/components/atoms/Avatar.tsx
@@ -0,0 +1,20 @@
+import Image from "next/image";
+import React from "react";
+
+interface AvatarProps {
+  url?: string;
+}
+
+const Avatar: React.FC<AvatarProps> = ({ url }) => {
+  return (
+    <Image
+      alt="avatar"
+      src={url || "/images/ava.png"}
+      width={48}
+      height={48}
+      className="object-cover rounded-full w-12 h-12"
+    />
+  );
+};
+
+export default Avatar;
