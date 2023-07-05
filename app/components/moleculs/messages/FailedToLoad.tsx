6efde34a5c5e1243163bebbb7ccda840e8203fa5
anggihnurh <works.anggihnur@gmail.com>
2023-07-05 11:20:51 +0700

    add messages column components

---
+++ app/components/moleculs/messages/FailedToLoad.tsx
@@ -0,0 +1,24 @@
+import { FailedIcon, RefreshIcon } from "@/public/icons/outline";
+import React from "react";
+import { Button } from "../../atoms";
+
+interface FailedToLoadProps {
+  onReload: () => void;
+}
+
+const FailedToLoad: React.FC<FailedToLoadProps> = ({ onReload }) => {
+  return (
+    <div className="flex flex-col gap-2 items-center w-64">
+      <FailedIcon width={80} height={80} />
+      <p className="text-[#8B9EB7]">Failed To Load Data</p>
+      <Button
+        type="subtle"
+        label="Reload"
+        Icon={RefreshIcon}
+        onClick={onReload}
+      />
+    </div>
+  );
+};
+
+export default FailedToLoad;
