4348f60ac761aa513778fa1bb4feb1da96ad9031
anggihnurh <works.anggihnur@gmail.com>
2023-07-07 04:38:51 +0700

    fix next config file

--- next.config.js
+++ next.config.js
@@ -1,16 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-<<<<<<< Updated upstream
     remotePatterns: [
       {
         protocol: "https",
         hostname: "**",
       },
     ],
-=======
-    domains: ["img.freepik.com"],
->>>>>>> Stashed changes
   },
 };
 
