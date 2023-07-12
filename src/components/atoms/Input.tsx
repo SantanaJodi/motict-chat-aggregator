1bfc9e39594c0616b8a1df24051d5f5cb9edd2a4
anggihnurh <works.anggihnur@gmail.com>
2023-07-12 10:38:53 +0700

    style width to full in input component

--- src/components/atoms/Input.tsx
+++ src/components/atoms/Input.tsx
@@ -14,7 +14,7 @@ const Input: React.FC<InputProps> = ({
   Icon,
 }) => {
   return (
-    <div className="bg-[#EEF5FF] rounded-lg flex flex-row items-center gap-2 p-2">
+    <div className="bg-[#EEF5FF] rounded-lg flex flex-row items-center gap-2 p-2 w-full">
       {Icon && <Icon />}
       <input
         className="w-full bg-[#EEF5FF] border-none focus:outline-none text-base placeholder:text-[#AABDD7]"
