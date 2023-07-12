eab86c9336718600ff5d00cdf245b4e06a89e984
anggihnurh <works.anggihnur@gmail.com>
2023-07-12 10:38:09 +0700

    styling button

--- src/components/atoms/buttons/Button.tsx
+++ src/components/atoms/buttons/Button.tsx
@@ -6,7 +6,7 @@ import React from "react";
 interface ButtonProps {
   type: "primary" | "subtle" | "ghost" | "link";
   Icon?: React.ElementType;
-  label: string;
+  label?: string;
   onClick: () => void;
   isLoading?: boolean;
   disabled?: boolean;
@@ -24,34 +24,36 @@ const Button: React.FC<ButtonProps> = ({
   color,
   size = "medium",
 }) => {
+  const iconSize = size === "medium" && type !== "link" ? 24 : 16;
   return (
     <button
       onClick={onClick}
       disabled={isLoading || disabled}
       className={clsx(
-        "rounded-lg gap-1 w-full",
-        !!Icon && "flex flex-row items-center justify-center gap-2",
+        "rounded-lg gap-1 w-max",
+        !!Icon && "flex flex-row items-center justify-center gap-1",
         type === "primary" && `bg-[${color}]`,
         type === "subtle" && "border border-[#D7E4F5] hover:bg-[#EEF5FF]",
         type === "ghost" && "hover:bg-[#EEF5FF]",
         type === "link" ? `w-max p-0` : "p-2"
       )}
     >
-      {Icon && (
-        <Icon className={clsx(size === "medium" ? "w-6 h-6" : "w-4 h-4")} />
+      {Icon && <Icon width={iconSize} height={iconSize} fill={color} />}
+      {label && (
+        <p
+          className={clsx(
+            "font-medium",
+            type === "primary" && "text-white",
+            type === "link" &&
+              `text-[${color}] underline text-[14px] leading-[18.23px]   font-normal`,
+            size === "medium" && type !== "link" && "text-[16px]",
+            size === "small" && "text-[14px]"
+          )}
+          style={type === "link" ? { color } : {}}
+        >
+          {label}
+        </p>
       )}
-      <p
-        className={clsx(
-          "font-medium",
-          type === "primary" && "text-white",
-          type === "link" && ` text-[${color}] underline font-normal`,
-          size === "medium" && "text-[16px]",
-          size === "small" && "text-[14px]"
-        )}
-        style={type === "link" ? { color } : {}}
-      >
-        {label}
-      </p>
     </button>
   );
 };
