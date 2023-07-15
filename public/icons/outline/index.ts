41933a7ff9a9ea4c593aa57976e1ca876ae88523
anggihnurh <works.anggihnur@gmail.com>
2023-07-15 16:11:58 +0700

    add some outline icons

--- public/icons/outline/index.ts
+++ public/icons/outline/index.ts
@@ -2,6 +2,7 @@ import AttachmentIcon from "./AttachmentIcon";
 import BroadcastIcon from "./BroadcastIcon";
 import ChartIcon from "./ChartIcon";
 import ChatIcon from "./ChatIcon";
+import ChevronDownIcon from "./ChevronDownIcon";
 import ChevronLeftIcon from "./ChevronLeftIcon";
 import ChevronRightIcon from "./ChevronRightIcon";
 import CircleCheckIcon from "./CircleCheckIcon";
@@ -15,14 +16,19 @@ import FilterIcon from "./FilterIcon";
 import HeadsetIcon from "./HeadsetIcon";
 import HelpIcon from "./HelpIcon";
 import ImageIcon from "./ImageIcon";
+import InfoIcon from "./InfoIcon";
+import MergeIcon from "./MergeIcon";
 import NotificationIcon from "./NotificationIcon";
 import PaperPlaneIcon from "./PaperPlaneIcon";
+import PenIcon from "./PenIcon";
 import PeoplesIcon from "./PeoplesIcon";
+import PersonCrossIcon from "./PersonCrossIcon";
 import PlayIcon from "./PlayIcon";
 import ProfileIcon from "./ProfileIcon";
 import RefreshIcon from "./RefreshIcon";
 import ReplyIcon from "./ReplyIcon";
 import ResolveIcon from "./ResolveIcon";
+import SaveIcon from "./SaveIcon";
 import SearchIcon from "./SearchIcon";
 import SettingsIcon from "./SettingsIcon";
 import SnackbarErrorIcon from "./SnackbarErrorIcon";
@@ -32,6 +38,12 @@ import TagIcon from "./TagIcon";
 import XIcon from "./XIcon";
 
 export {
+  ChevronDownIcon,
+  SaveIcon,
+  InfoIcon,
+  PersonCrossIcon,
+  PenIcon,
+  MergeIcon,
   ImageIcon,
   FileIcon,
   PlayIcon,
