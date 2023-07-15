f9833c255df9f574d5f3f0003ffa01989cbb80bd
anggihnurh <works.anggihnur@gmail.com>
2023-07-15 16:17:55 +0700

    add modal component

---
+++ src/components/atoms/Modal.tsx
@@ -0,0 +1,37 @@
+import { ModalType } from "@/src/types";
+import clsx from "clsx";
+import React, { PropsWithChildren } from "react";
+import { ModalHeader } from "../molecules";
+
+interface ModalProps extends ModalType, PropsWithChildren {
+  footer?: React.ReactNode;
+}
+
+const Modal: React.FC<ModalProps> = ({
+  title,
+  onClose,
+  visible,
+  children,
+  footer,
+}) => {
+  return (
+    <div
+      className={clsx(
+        "fixed top-0 left-0 w-full min-h-screen bg-[#0D0F12A1] flex items-center justify-center z-50",
+        visible ? "block" : "hidden"
+      )}
+    >
+      <div className="bg-white w-[480px] h-[560px] rounded-2xl relative overflow-hidden flex flex-col">
+        <ModalHeader title={title} onClose={onClose} />
+        <div className="flex flex-col justify-between h-[calc(100%_-_65px)]">
+          <div className="px-6 pt-6 h-[calc(100%_-_72px)]">{children}</div>
+          <div className="px-6 py-4 bg-white border-t border-[#EEF5FF] w-full z-[1]">
+            {footer}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
