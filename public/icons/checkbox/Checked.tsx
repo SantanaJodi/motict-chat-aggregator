f58dc0ff6b40d0a062e3a64e48bd697efa69ec60
anggihnurh <works.anggihnur@gmail.com>
2023-07-18 16:57:25 +0700

    ad checkbox icon

---
+++ public/icons/checkbox/Checked.tsx
@@ -0,0 +1,13 @@
+import { SVGProps } from "react";
+
+const Checked = (props: SVGProps<SVGSVGElement>) => (
+  <svg width={24} height={24} viewBox="0 0 24 24" fill="none" {...props}>
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M21.1155 6.84265L10.6004 19.0477L2.89819 13.1961L4.71305 10.8073L10.1689 14.9523L18.8427 4.88452L21.1155 6.84265Z"
+      fill="white"
+    />
+  </svg>
+);
+export default Checked;
