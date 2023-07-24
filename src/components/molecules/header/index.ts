227e1d9dea440aef717308f57478294c5bb574e0
anggihnurh <works.anggihnur@gmail.com>
2023-07-24 09:55:54 +0700

    add detail contact header

--- src/components/molecules/header/index.ts
+++ src/components/molecules/header/index.ts
@@ -1,11 +1,13 @@
 import ChatHeader from "./ChatHeader";
 import ChatroomHeader from "./ChatroomHeader";
 import ContactHeader from "./ContactHeader";
+import DetailContactHeader from "./DetailContactHeader";
 import ModalHeader from "./ModalHeader";
 import NewContactHeader from "./NewContactHeader";
 import SidebarHeader from "./SidebarHeader";
 
 export {
+  DetailContactHeader,
   NewContactHeader,
   ContactHeader,
   ModalHeader,
