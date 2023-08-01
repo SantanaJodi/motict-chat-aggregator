fdad6898a3264d27551c393fb0ca5ed8139a8074
anggihnurh <works.anggihnur@gmail.com>
2023-08-01 17:49:00 +0700

    add AccountManagementView

---
+++ src/modules/settings/view/AccountManagementView.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+interface AccountManagementViewProps {}
+
+const AccountManagementView: React.FC<AccountManagementViewProps> = () => {
+  return <div className="w-full h-full">Account Management</div>;
+};
+
+export default AccountManagementView;
