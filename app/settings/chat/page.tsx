39286809881ab80dbfa72debd51f82ded06b2bd1
anggihnurh <works.anggihnur@gmail.com>
2023-08-01 17:48:15 +0700

    add settings page and the sub page

---
+++ app/settings/chat/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+interface ChatProps {}
+
+const Chat: React.FC<ChatProps> = () => {
+  return <h1>CHAT SETTINGS</h1>;
+};
+
+export default Chat;
---
+++ app/settings/integration/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+interface WhatsAppIntegrationProps {}
+
+const WhatsAppIntegration: React.FC<WhatsAppIntegrationProps> = () => {
+  return <h1>WhatsApp Integration</h1>;
+};
+
+export default WhatsAppIntegration;
