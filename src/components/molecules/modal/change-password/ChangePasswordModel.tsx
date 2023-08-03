f6cfeced03e92c9105c87c40b39402f20a8ec422
anggihnurh <works.anggihnur@gmail.com>
2023-08-03 17:55:10 +0700

    add validation and onClose function

--- src/components/molecules/modal/change-password/ChangePasswordModel.tsx
+++ src/components/molecules/modal/change-password/ChangePasswordModel.tsx
@@ -1,20 +1,47 @@
+import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { toast } from "react-hot-toast";
-
+import * as z from "zod";
 interface IChangePassword {
   old: string;
   new: string;
   confirm: string;
 }
 
-const ChangePasswordModel = () => {
+interface ChangePasswordModelProps {
+  onClose: () => void;
+}
+
+const schema = z
+  .object({
+    old: z.string(),
+    new: z
+      .string()
+      .regex(
+        /([A-Za-z]+[0-9]|[0-9]+[A-Za-z])[A-Za-z0-9]*/,
+        "Use a combination of letters and numbers"
+      ),
+    confirm: z.string(),
+  })
+  .refine((data) => data.new === data.confirm, {
+    message: "Password not matched!",
+    path: ["confirm"],
+  });
+
+const ChangePasswordModel = ({ onClose }: ChangePasswordModelProps) => {
   const formModule = useForm<IChangePassword>({
     defaultValues: {},
+    resolver: zodResolver(schema),
   });
 
   const onChangePassword = (val: IChangePassword) => {
-    console.log("🚀 onChangePassword value -> ", val);
+    const oldPass = "123";
+
+    if (val.old !== oldPass) {
+      return formModule.setError("old", { message: "Wrong Password" });
+    }
     toast.success("Password Successfully Changed");
+    onClose();
   };
 
   return { formModule, onChangePassword };
