3975660ecbc0028cd5bd3093a9a4f0fa7dc8e323
anggihnurh <works.anggihnur@gmail.com>
2023-08-07 13:56:19 +0700

    AddWhatsAppAccount page

---
+++ app/settings/integration/add/page.tsx
@@ -0,0 +1,7 @@
+import AddWhatsAppAccountView from "@/src/modules/settings/view/integration/AddWhatsAppAccountView";
+
+const AddWhatsAppAccountPage = () => {
+  return <AddWhatsAppAccountView />;
+};
+
+export default AddWhatsAppAccountPage;
