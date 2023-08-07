0b4f1aec60c1f5dc6a004a0aa3122af7ae32ce9b
anggihnurh <works.anggihnur@gmail.com>
2023-08-07 15:40:40 +0700

    WhatsAppAccountSetting view & page

---
+++ app/settings/integration/[whatsappNumber]/page.tsx
@@ -0,0 +1,7 @@
+import WhatsAppAccountSettingView from "@/src/modules/settings/view/integration/WhatsAppAccountSettingView";
+
+const WhatsAppAccountSettingPage = () => {
+  return <WhatsAppAccountSettingView />;
+};
+
+export default WhatsAppAccountSettingPage;
