c43a31199e244f11446e32de822ba89aca28ca6e
anggihnurh <works.anggihnur@gmail.com>
2023-08-07 11:42:36 +0700

    add IntegrationView

---
+++ src/modules/settings/view/IntegrationView.tsx
@@ -0,0 +1,46 @@
+/* eslint-disable react/no-unescaped-entities */
+"use client";
+
+import {
+  FailedToLoad,
+  IntegrationChannelCard,
+  IntegrationHeader,
+  Loading,
+} from "@/src/components";
+import clsx from "clsx";
+import React from "react";
+import IntegrationViewModel from "../model/IntegrationViewModel";
+
+interface IntegrationViewProps {}
+
+const IntegrationView: React.FC<IntegrationViewProps> = () => {
+  const { toggle, isLoading, isError, handleToggle, refetch } =
+    IntegrationViewModel();
+  return (
+    <div className="bg-white overflow-y-auto relative flex flex-col w-full h-full">
+      <IntegrationHeader />
+      <div
+        className={clsx(
+          "p-6 w-full h-full flex flex-col",
+          isLoading || isError ? "items-center justify-center" : "items-start"
+        )}
+      >
+        {isLoading ? (
+          <Loading />
+        ) : isError ? (
+          <FailedToLoad onReload={refetch} />
+        ) : (
+          <div className="flex flex-row items-center gap-8">
+            <IntegrationChannelCard
+              account="Wikitoko"
+              toggle={toggle}
+              onToggle={() => handleToggle(!toggle)}
+            />
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default IntegrationView;
