885966231cd97237eaf54ad85da46f4fb4c3e662
anggihnurh <works.anggihnur@gmail.com>
2023-08-09 09:31:53 +0700

    add disabled props

--- src/components/molecules/list/ListSetting.tsx
+++ src/components/molecules/list/ListSetting.tsx
@@ -4,13 +4,14 @@ import { FieldSwitch } from "../../atoms";
 interface ListSettingProps {
   name: string;
   label: string;
+  disabled?: boolean;
 }
 
-const ListSetting: React.FC<ListSettingProps> = ({ label, name }) => {
+const ListSetting: React.FC<ListSettingProps> = ({ label, name, disabled }) => {
   return (
     <div className="w-full flex flex-row items-center justify-between">
       <p className="text-sm text-[#0D0F12]">{label}</p>
-      <FieldSwitch name={name} />
+      <FieldSwitch name={name} disabled={disabled} />
     </div>
   );
 };
