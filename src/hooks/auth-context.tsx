7c560a316165201dcc3e9227c808e73f669b9324
anggihnurh <works.anggihnur@gmail.com>
2023-08-14 12:53:42 +0700

    add logout modal state and logout function

--- src/hooks/auth-context.tsx
+++ src/hooks/auth-context.tsx
@@ -1,31 +1,53 @@
 "use client";
 import { useRouter } from "next/navigation";
 
-import React, { PropsWithChildren, useEffect, useState } from "react";
+import React, { PropsWithChildren, useEffect } from "react";
 
 import Cookies from "js-cookie";
+import { useImmer } from "use-immer";
 
 const AuthContext = React.createContext<AuthContextProps>({} as any);
+
+interface IState {
+  token: string | null;
+  logoutModal: boolean;
+}
 
 export const AuthContextProvider: React.FC<PropsWithChildren> = ({
   children,
 }) => {
   const router = useRouter();
-  const [{ token }, setState] = useState<{
-    token: string | null;
-  }>({
+  const [{ token, logoutModal }, update] = useImmer<IState>({
     token: null,
+    logoutModal: false,
   });
+
+  const handleToken = (val: string | null) => {
+    update((s) => {
+      s.token = val;
+    });
+  };
 
   const handleSetToken = (token: string) => {
     Cookies.set("token", token);
-    setState({ token });
+    handleToken(token);
   };
 
   const handleRevokeToken = () => {
     Cookies.remove("token");
-    setState({ token: null });
+    handleToken(null);
     router.replace("/login");
+  };
+
+  const handleLogoutModal = (val: boolean) => {
+    update((s) => {
+      s.logoutModal = val;
+    });
+  };
+
+  const handleLogout = () => {
+    handleRevokeToken();
+    handleLogoutModal(false);
   };
 
   useEffect(() => {
@@ -33,12 +55,22 @@ export const AuthContextProvider: React.FC<PropsWithChildren> = ({
     if (!cookieToken) {
       router.replace("/login");
     } else {
-      setState({ token: cookieToken });
+      handleToken(cookieToken);
     }
-  }, [router, setState]);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [router]);
 
   return (
-    <AuthContext.Provider value={{ token, handleRevokeToken, handleSetToken }}>
+    <AuthContext.Provider
+      value={{
+        token,
+        logoutModal,
+        handleRevokeToken,
+        handleSetToken,
+        handleLogoutModal,
+        handleLogout,
+      }}
+    >
       {children}
     </AuthContext.Provider>
   );
@@ -48,6 +80,9 @@ export const useAuthContext = () => React.useContext(AuthContext);
 
 export interface AuthContextProps {
   token: string | null;
+  logoutModal: boolean;
   handleSetToken: (token: string) => void;
+  handleLogoutModal: (val: boolean) => void;
   handleRevokeToken: () => void;
+  handleLogout: () => void;
 }
