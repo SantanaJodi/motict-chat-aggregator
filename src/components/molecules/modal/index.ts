99595d84aaf032bff4eb737644720be385780e19
anggihnurh <works.anggihnur@gmail.com>
2023-08-14 10:38:08 +0700

    add unsaved modal

--- src/components/molecules/modal/index.ts
+++ src/components/molecules/modal/index.ts
@@ -1,6 +1,11 @@
 import ChatTemplatesModal from "./ChatTemplatesModal";
 import DeleteRecipientGroupModal from "./DeleteRecipientGroupModal";
+import UnsavedConfirmationModal from "./UnsavedConfirmationModal";
 
 export * from "./agent";
 export * from "./change-password";
-export { ChatTemplatesModal, DeleteRecipientGroupModal };
+export {
+  ChatTemplatesModal,
+  UnsavedConfirmationModal,
+  DeleteRecipientGroupModal,
+};
