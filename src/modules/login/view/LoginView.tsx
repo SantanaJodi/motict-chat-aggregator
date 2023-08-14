a9a432eeff3afc4b8978d9fe72d89e7e6d31c679
anggihnurh <works.anggihnur@gmail.com>
2023-08-14 12:52:41 +0700

    fix login view

--- src/modules/login/view/LoginView.tsx
+++ src/modules/login/view/LoginView.tsx
@@ -1,48 +1,55 @@
 "use client";
-import { EmojiIcon } from "@/public/icons/outline";
+import { BrandLogoType } from "@/public/icons/logo";
 import { Button } from "@/src/components";
-import { FieldInput } from "@/src/components/atoms/fields";
-import { useState } from "react";
+import { FieldInput, FieldInputPassword } from "@/src/components/atoms/fields";
+import Image from "next/image";
 import { FormProvider } from "react-hook-form";
+import { ClipLoader } from "react-spinners";
 import { LoginViewModel } from "../view-model/login-view-model";
 
 export const LoginView: React.FC = () => {
-  const { formModule, handleLogin } = LoginViewModel();
-  const [visible, setVisible] = useState(false);
+  const { formModule, isLoading, handleLogin } = LoginViewModel();
 
   return (
-    <div className="w-full h-full flex items-center justify-center">
-      <div
-        className=" w-[400px] p-6 rounded-lg bg-white border border-[#EEF5FF]"
-        style={{ boxShadow: "0px 0px 16px 0px rgba(0, 0, 0, 0.16)" }}
-      >
-        <h1 className="font-bold mb-8 text-center">MOTICT - Chat Agregator</h1>
+    <div className="w-full h-full flex items-center justify-center relative">
+      <Image
+        fill
+        src="/images/svg/big-logo.svg"
+        alt="background image"
+        className="absolute top-0 !-left-80"
+      />
+      <div className="w-[400px] p-6 rounded-lg bg-white border border-[#EEF5FF] z-10">
         <FormProvider {...formModule}>
           <form onSubmit={formModule.handleSubmit(handleLogin)}>
-            <div className="flex flex-col gap-6">
-              <FieldInput
-                name="email"
-                placeholder="Masukkan email disini.."
-                disableClear
-              />
-              <FieldInput
-                name="password"
-                placeholder="Masukkan password disini.."
-                type={visible ? "text" : "password"}
-                rightElement={
-                  <EmojiIcon
-                    fill={visible ? "#C02716" : "#0D0F12"}
-                    onClick={() => setVisible((prev) => !prev)}
-                  />
-                }
-              />
-              <Button
-                type="submit"
-                label="Login"
-                variant="primary"
-                color="#323944"
-                className="!w-full cursor-pointer"
-              />
+            <div className="flex flex-col gap-10">
+              <BrandLogoType />
+              <div className="flex flex-col gap-6">
+                <FieldInput
+                  name="email"
+                  label="Email"
+                  placeholder="Masukkan email disini.."
+                  disableClear
+                />
+                <FieldInputPassword
+                  label="Password"
+                  name="password"
+                  placeholder="Masukkan password disini.."
+                />
+              </div>
+              {isLoading ? (
+                <div className="flex gap-2 items-center justify-center">
+                  <ClipLoader color="#C02716" size={20} />
+                  <p className="text-[#8B9EB7] text-center">Logging In</p>
+                </div>
+              ) : (
+                <Button
+                  type="submit"
+                  label="Login"
+                  variant="primary"
+                  color="#323944"
+                  className="!w-full cursor-pointer"
+                />
+              )}
             </div>
           </form>
         </FormProvider>
