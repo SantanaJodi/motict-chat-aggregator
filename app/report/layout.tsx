9c30676333d6ac27b8f78d3cd306e250408c049a
anggihnurh <works.anggihnur@gmail.com>
2023-08-16 05:46:06 +0700

    add report layout view

---
+++ app/report/layout.tsx
@@ -0,0 +1,21 @@
+"use client";
+import { Container, ReportHeader } from "@/src";
+import { ReportContextProvider } from "@/src/modules/report/context/ReportContext";
+import React, { PropsWithChildren } from "react";
+
+interface ReportViewLayoutProps extends PropsWithChildren {}
+
+const ReportViewLayout: React.FC<ReportViewLayoutProps> = ({ children }) => {
+  return (
+    <Container>
+      <ReportContextProvider>
+        <div className="w-full h-full bg-white overflow-y-auto">
+          <ReportHeader />
+          <div className="w-full h-full">{children}</div>
+        </div>
+      </ReportContextProvider>
+    </Container>
+  );
+};
+
+export default ReportViewLayout;
