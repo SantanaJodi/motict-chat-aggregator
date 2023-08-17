ce539024025080bac6749c95bc843ad40a15cd5d
anggihnurh <works.anggihnur@gmail.com>
2023-08-18 04:27:29 +0700

    add agent performance view, type, dummy and model

---
+++ src/modules/report/view/ReportAgentPerformanceView.tsx
@@ -0,0 +1,57 @@
+"use client";
+
+import {
+  ChatTime,
+  FailedToLoad,
+  Loading,
+  TableAverageFirstResponseTimePerAgent,
+  TableAverageResolveTimePerAgent,
+  TableAverageResponseTimePerAgent,
+  TableNewConversationPerAgent,
+  TableResolvedConversationPerAgent,
+} from "@/src/components";
+import React from "react";
+import ReportAgentPerformanceViewModel from "../model/ReportAgentPerformanceViewModel";
+
+interface ReportAgentPerformanceViewProps {}
+
+const ReportAgentPerformanceView: React.FC<
+  ReportAgentPerformanceViewProps
+> = () => {
+  const { data, isError, isLoading, refetch } =
+    ReportAgentPerformanceViewModel();
+
+  if (isLoading || isError) {
+    return (
+      <div className="w-full h-full flex flex-col justify-center items-center">
+        {isLoading ? <Loading /> : <FailedToLoad onReload={refetch} />}
+      </div>
+    );
+  }
+
+  return (
+    <div className="w-full h-full flex flex-col gap-6 p-6">
+      <ChatTime time="Conversation per Agent" />
+      <div className="grid grid-cols-2 gap-4">
+        <TableNewConversationPerAgent data={data?.new_conversation_per_agent} />
+        <TableResolvedConversationPerAgent
+          data={data?.resolved_conversation_per_agent}
+        />
+      </div>
+      <ChatTime time="Miscellaneous" />
+      <div className="grid grid-cols-2 gap-4">
+        <TableAverageResolveTimePerAgent
+          data={data?.average_resolve_time_per_agent}
+        />
+        <TableAverageResponseTimePerAgent
+          data={data?.average_resolve_time_per_agent_week}
+        />
+      </div>
+      <TableAverageFirstResponseTimePerAgent
+        data={data?.average_first_response_time_per_agent}
+      />
+    </div>
+  );
+};
+
+export default ReportAgentPerformanceView;
