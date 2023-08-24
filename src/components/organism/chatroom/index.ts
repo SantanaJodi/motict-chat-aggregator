ba195e480f7279b60aa6a76a8efb37541cefa103
anggihnurh <works.anggihnur@gmail.com>
2023-08-25 00:07:46 +0700

    re export message type

--- src/components/organism/chatroom/index.ts
+++ src/components/organism/chatroom/index.ts
@@ -2,4 +2,5 @@ import ChatroomComponent from "./chatroom/ChatroomComponent";
 import ChatroomDetail from "./ChatroomDetail";
 import Messages from "./messages/Messages";
 
-export { Messages, ChatroomComponent as Chatroom, ChatroomDetail };
+export * from "./messages/types/MessagesTypes";
+export { Messages, ChatroomComponent, ChatroomDetail };
