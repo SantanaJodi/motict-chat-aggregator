658b342a77064112e0d3d734b1337115ccdddb14
anggihnurh <works.anggihnur@gmail.com>
2023-08-24 15:53:40 +0700

    dummy integration whatsapp account list

--- app/settings/integration/page.tsx
+++ app/settings/integration/page.tsx
@@ -1,10 +1,41 @@
-import IntegrationView from "@/src/modules/settings/view/integration/IntegrationView";
-import React from "react";
+import { IntegrationView } from "@/src/modules/settings";
 
-interface WhatsAppIntegrationProps {}
+const getWhatsAppAccountList = async () => {
+  // API: list for Whatsapp Account that already integrated
+  return [
+    {
+      badge: "",
+      token: "isdfj894509hfsfsad928fndasi898978bdlkw4md89",
+      whatsappName: "Wikitoko",
+      whatsappNumber: "081234567890",
+      whatsappServerUrl: "https://graph.facebook.com",
+      isSupportSSL: false,
+      isActive: false,
+    },
+    {
+      badge: "",
+      token: "akfjno7h34ro783hfasuilfhlanaho873tra349",
+      whatsappName: "TokoWiki",
+      whatsappNumber: "089234892333",
+      whatsappServerUrl: "https://graph.facebook.com",
+      isSupportSSL: false,
+      isActive: true,
+    },
+    {
+      badge: "",
+      token: "asdadn2873r23fsadfn2a",
+      whatsappName: "Toko Toko",
+      whatsappNumber: "089234234234",
+      whatsappServerUrl: "https://graph.facebook.com",
+      isSupportSSL: false,
+      isActive: true,
+    },
+  ];
+};
 
-const WhatsAppIntegration: React.FC<WhatsAppIntegrationProps> = () => {
-  return <IntegrationView />;
+const WhatsAppIntegration = async () => {
+  const accounts = await getWhatsAppAccountList();
+  return <IntegrationView accounts={accounts} />;
 };
 
 export default WhatsAppIntegration;
