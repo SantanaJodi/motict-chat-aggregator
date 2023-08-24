f50f6ec8f0c732458b999efb0de4ed3cf82a871d
anggihnurh <works.anggihnur@gmail.com>
2023-08-25 00:11:19 +0700

    add message status tab component

---
+++ src/components/molecules/tab/MessageStatusTab.tsx
@@ -0,0 +1,47 @@
+import { StatusEnumTranslator } from "@/src/utils";
+import { groupBy } from "lodash";
+import React, { useCallback } from "react";
+import { Filter } from "../../atoms/tag";
+import { IChatroomDetail, StatusEnum } from "../../organism";
+
+interface MessageStatusTabProps {
+  messages?: IChatroomDetail[];
+  status?: string;
+  onChangeStatus: (value: StatusEnum) => void;
+}
+
+const MessageStatusTab: React.FC<MessageStatusTabProps> = ({
+  onChangeStatus,
+  messages,
+  status,
+}) => {
+  const getLabel = useCallback(
+    (item: StatusEnum) => {
+      if (item === StatusEnum.WAITING || item === StatusEnum.ASSIGNED_TO_ME) {
+        const count = groupBy(messages, "status")[item]?.length;
+        return (
+          StatusEnumTranslator(item) +
+          (count > 0 ? ` • ${count <= 99 ? count : "+99"}` : "")
+        );
+      }
+
+      return StatusEnumTranslator(item);
+    },
+    [messages]
+  );
+
+  return (
+    <div className="flex flex-row items-center gap-2 overflow-x-auto  px-4 pb-4 no-scrollbar flex-shrink-0">
+      {Object.values(StatusEnum).map((item) => (
+        <Filter
+          key={item}
+          label={getLabel(item)}
+          onClick={() => onChangeStatus(item)}
+          isActive={status === item}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default MessageStatusTab;
